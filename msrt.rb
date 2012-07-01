b46e985127619701d83ff6362f03444277c9e6af
Nicholas Devenish <ndevenish@gmail.com>
2012-07-01 16:21:00 +0100

    Give MSRT a version number.

--- msrt.rb
+++ msrt.rb
@@ -3,7 +3,9 @@ require 'formula'
 class Msrt < Formula
   homepage 'http://www-numi.fnal.gov/offline_software/srt_public_context/WebDocs/msrt.html'
   head 'cvs://:pserver:anonymous:anoncvs@minoscvs.fnal.gov:/cvs/minoscvs/rep1:setup/msrt'
+  url 'cvs://:pserver:anonymous:anoncvs@minoscvs.fnal.gov:/cvs/minoscvs/rep1:setup/msrt'
   sha1 '8dd80f582149052c87907f675ec3089fc3dc7a2d'
+  version "1.0"  
 
   def install
     # Build the man page from help
