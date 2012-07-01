520fbf77449681bfb0fa822b4f276bcd8ef1dda7
Nicholas Devenish <ndevenish@gmail.com>
2012-07-01 13:14:07 +0100

    Patch out the password requests

--- msrt.rb
+++ msrt.rb
@@ -9,4 +9,37 @@ class Msrt < Formula
     bin.install "msrt"
   end
 
+  def patches
+    # Remove the completely unnecessary "type" prompts
+    DATA
+  end
 end
+
+__END__
+diff --git a/msrt b/msrt
+index 0918a21..02dc2f9 100755
+--- a/msrt
++++ b/msrt
+@@ -10,9 +10,9 @@
+ 
+ # Some hard coded values unlikely to change very frequently:
+ 
+-srt_cvsroot=":pserver:anonymous@cdcvs.fnal.gov:/srtcvs"
++srt_cvsroot=":pserver:anonymous:anoncvs@cdcvs.fnal.gov:/srtcvs"
+ 
+-cvsroot_pserver=":pserver:anonymous@minoscvs.fnal.gov:/cvs/minoscvs/rep1"
++cvsroot_pserver=":pserver:anonymous:anoncvs@minoscvs.fnal.gov:/cvs/minoscvs/rep1"
+ cvsroot_ssh="minoscvs@minoscvs.fnal.gov:/cvs/minoscvs/rep1"
+ 
+ ########################################################################
+@@ -414,9 +414,6 @@ anoncvs () {
+ 
+     grep -q $cvsroot $HOME/.cvspass > /dev/null 2>&1
+     if [ $? -ne 0 ] ; then
+-	echo
+-	echo "Please type \"anoncvs\" at the \"CVS password:\" prompt."
+-	echo
+         cvs -d $cvsroot login
+     fi
+     if [ $? != 0 ] ; then 
+
